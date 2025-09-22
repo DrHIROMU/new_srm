60013469e1cbdaad89990a7fb80d321b30bdd378
KHLin <drhiromu@gmail.com>
2025-09-22 22:27:11 +0800

    fix(config): add scanBasePackages to enable proper component scanning

--- back-end/srm/srm-scheduler/src/main/java/com/advantech/srm/scheduler/SrmSchedulerApplication.java
+++ back-end/srm/srm-scheduler/src/main/java/com/advantech/srm/scheduler/SrmSchedulerApplication.java
@@ -3,7 +3,12 @@ package com.advantech.srm.scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.advantech.srm.scheduler",
+                "com.advantech.srm.persistence"
+        }
+)
 public class SrmSchedulerApplication {
 
 	public static void main(String[] args) {
